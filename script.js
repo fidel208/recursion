9bb7b6a7543ff414d3f1e9da858c52911329293a
fidel208 <dempafidel@gmail.com>
2025-10-23 12:03:33 +0300

    Done with the first step

---
+++ script.js
@@ -0,0 +1,26 @@
+function fibs(n) {
+    let sequence = [0, 1];
+
+    for (let i = 2; i < n; i++) {
+        sequence.push(sequence[i -1] + sequence[i -2]);
+    }
+
+    return sequence.slice(0, n);
+}
+
+console.log(fibs(10));
+
+function fibsRec(n) {
+    if (n <= 1) {
+        return[0];
+    }
+    if (n === 2) {
+        return[0, 1]
+    }
+
+    let sequence = fibsRec(n -1);
+    sequence.push(sequence[sequence.length -1 ] + sequence[sequence.length - 2]);
+    return sequence;
+}
+
+console.log(fibsRec(10));
